7f6847c2ef6c3cf2de5b49f1da1b66111aa0245d
MMarzoo <mmarzouk12378@gmail.com>
2024-01-22 10:41:25 -0800

    creating file

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -5,6 +5,6 @@ def safe_print_integer_err(value):
     try:
         print("{:d}".format(value))
     except Exception as e:
-        pritn("Exception:", e, file=sys.stderr)
+        print("Exception:", e, file=sys.stderr)
         is_int = False
     return is_int
