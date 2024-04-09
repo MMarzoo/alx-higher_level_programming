3c34e60bcccad992c167f998f8cbc215d077012a
MMarzoo <mmarzouk12378@gmail.com>
2024-04-09 08:06:38 -0700

    adding files

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,17 @@
 #!/usr/bin/node
 const dict = require('./101-data').dict;
 
-const totallist = Object.entries(dict);
+const totalist = Object.entries(dict);
 const vals = Object.values(dict);
 const valsUniq = [...new Set(vals)];
 const newDict = {};
 for (const j in valsUniq) {
-  const list = {};
+  const list = [];
   for (const k in totalist) {
-    if (totallist[k][1] === valsUniq[j]) {
-      list.unshift(totallist[k][0]);
+    if (totalist[k][1] === valsUniq[j]) {
+      list.unshift(totalist[k][0]);
     }
   }
-  newDict[valsUniq[j] = list;
+  newDict[valsUniq[j]] = list;
 }
 console.log(newDict);
