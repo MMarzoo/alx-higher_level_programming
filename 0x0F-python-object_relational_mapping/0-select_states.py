cfd2947edb10fd14e65715bf971ef47864413153
MMarzoo <mmarzouk12378@gmail.com>
2024-04-14 08:11:29 -0700

    adding files

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
     from the database.
     """
     db = MySQLdb.connect(host="localhost", user=argv[1], port=3306,
-                         paaswd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3])
 
-    cur =db.cursor()
+    cur = db.cursor()
     cur.execute("SELECT * FROM states")
     rows = cur.fetchall()
 
