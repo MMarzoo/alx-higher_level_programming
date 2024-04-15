04ae7f193af6561db2fad4ad3b8ae6bcf462efb0
MMarzoo <mmarzouk12378@gmail.com>
2024-04-15 13:24:12 -0700

    adding files

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -23,7 +23,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
 
     session = Session()
-    cal_state = State(name'California')
+    cal_state = State(name='California')
     sfr_city = City(name='San Francisco')
     cal_state.cities.append(sfr_city)
 
