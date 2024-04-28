ff13822e4cfef7c8fdfd5c6312f5eea3bc3d6df6
MMarzoo <mmarzouk12378@gmail.com>
2024-04-28 11:51:38 -0700

    adding files

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -9,15 +9,15 @@ import requests
 
 
 if __name__ == "__main__":
-    let = "" if len(sys.argv) == 1 else sys.argv[1]
-    payload = {"q": let}
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    payload = {"q": letter}
 
-    r = request.post("http://0.0.0.0:5000/search_user", data=payload)
+    r = requests.post("http://0.0.0.0:5000/search_user", data=payload)
     try:
         response = r.json()
         if response == {}:
             print("No result")
         else:
-            print("[{}] {}".format(response.get("id"), response("name")))
+            print("[{}] {}".format(response.get("id"), response.get("name")))
     except ValueError:
         print("Not a valid JSON")
