4b4f4c1b0a8c196096ba79444dbf6fa2c74845b5
MMarzoo <mmarzouk12378@gmail.com>
2024-04-28 12:05:41 -0700

    adding files

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -9,10 +9,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/{}/{}/commits".format(
         sys.argv[2], sys.argv[1])
 
-    res = requests.get(url)
-    commits = res.json()
+    r = requests.get(url)
+    commits = r.json()
     try:
-        fir i in range(10):
+        for i in range(10):
             print("{}: {}".format(
                 commits[i].get("sha"),
                 commits[i].get("commit").get("author").get("name")))
