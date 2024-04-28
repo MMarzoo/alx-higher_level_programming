6a60e6d00f719d21603b6163c3a1df212ab22356
MMarzoo <mmarzouk12378@gmail.com>
2024-04-28 08:15:27 -0700

    adding files

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,8 +2,9 @@
 """fetches https://intranet.hbtn.io/status."""
 import requests
 
+
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
-    print("Body response")
+    print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
