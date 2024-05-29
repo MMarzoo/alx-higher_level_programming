3c083d1617ccc254bd1b0496057d24626f432d32
MMarzoo <marzouk12378@gmail.com>
2024-05-30 00:39:50 +0300

    adding files

--- 0x15-javascript-web_jquery/103-script.js
+++ 0x15-javascript-web_jquery/103-script.js
@@ -1,19 +1,21 @@
 // fetches and prints how to say “Hello” depending on the language
 
-$('document').ready(function () {
-    $('INPUT#btn_translate').click(translate);
-    $('INPUT#language_code').focus(function () {
-      $(this).keydown(function (e) {
-        if (e.keyCode === 13) {
-          translate();
-        }
-      });
+$(document).ready(function () {
+  $('#btn_translate').click(translate);
+  $('INPUT#language_code').focus(function () {
+    $(this).keydown(function (e) {
+      if (e.keyCode === 13) {
+        translate();
+      }
     });
   });
+});
 
-  function translate () {
-    const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
-    $.get(url + $.param({ lang: $('INPUT#language_code').val() }), function (data) {
-      $('DIV#hello').html(data.hello);
-    });
-  }
+function translate () {
+  const url = 'https://hellosalut.stefanbohacek.dev/?lang=';
+  $('#hello').empty();
+  const len = $('#language_code').val();
+  $.get(url + len, function (response) {
+    $('#hello').append(response.hello);
+  });
+}
