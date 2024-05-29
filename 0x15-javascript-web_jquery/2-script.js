3e353b00e2bd50c1462be6463943d3dc94764db0
MMarzoo <marzouk12378@gmail.com>
2024-05-29 19:00:22 +0300

    adding files

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,5 +1,5 @@
 //  updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header
 
-$('div#red_header').click(function () {
-    $('header').css('color', '#FF0000');
-  });
+$('DIV#red_header').click(function () {
+  $('HEADER').css('color', '#FF0000');
+});
